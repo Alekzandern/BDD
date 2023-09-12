d59362f12e3d6b814d5d5aba9feec82756f9c334
Alexzander <Alex.Zander54.ru>
2023-09-12 23:14:22 +0000

    first commit

---
+++ src/test/java/ru.netology/page/DashboardPage.java
@@ -0,0 +1,50 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+
+    private final ElementsCollection cards = $$(".list__item div");
+
+    public DashboardPage() {
+
+        SelenideElement heading = $("[data-test-id=dashboard]");
+        heading.shouldBe(visible);
+    }
+
+    public int getCardBalance(DataHelper.CardInfo cardInfo) {
+        String text;
+        text = cards.findBy(text(cardInfo.getCardNumber().substring(15))).getText();
+        return extractBalance(text);  // поиск баланса по номеру карты, последние 4 цифры
+        // берет строку и преобразует ее в число, возвращает значени числа баланса
+    }
+
+    public int getCardBalance(int index) {
+    var text = cards.get(index).getText();
+    return extractBalance(text);
+    }
+
+    public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
+        cards.findBy(attribute("data-test-id", cardInfo.getTestId())).$("button").click();
+        return new TransferPage();
+    }
+
+    private int extractBalance(String text) {
+        String balanceStart = "баланс: ";
+        val start = text.indexOf(balanceStart);
+        String balanceFinish = " р.";
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
+
+
+
---
+++ src/test/java/ru.netology/test/MoneyTransferTest.java
@@ -0,0 +1,45 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataHelper.*;
+
+public class MoneyTransferTest {
+
+    DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setup() {
+        Selenide.open("http://localhost:9999");
+        var authInfo = getAuthInfo();
+        var verificationPage = LoginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferFromFirstCardToSecond() {
+        var firstCardInfo = DataHelper.getFirstCartInfo(); // инфо о первой карте
+        var secondCardInfo = DataHelper.getSecondCartInfo(); // инфо о второй карте
+        var firstCardBalance = dashboardPage.getCardBalance(getFirstCartInfo()); //  поиск баланса по номеру карты, последние 4 цифры
+        var secondCardBalance = dashboardPage.getCardBalance(getSecondCartInfo()); // поиск баланса по номеру карты, последние 4 цифры
+        var amount = generateValidBalance(firstCardBalance); // генерирует валидный баланс
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo); // ищем карту по id, и нажимаем кнопку
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo); //перевод
+// Сравнение полученных данных
+        var expectedBalanceFirstCard = firstCardBalance - amount; //ожидаемый результат первой карты
+        var expectedBalanceSecondCard = secondCardBalance + amount; // ожидаемы результат второй карты
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+}
+
